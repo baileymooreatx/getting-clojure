fc1993c8140ece5fa8571040a6a8032cd080b69a
baileymooreatx <sbaileymoore@yahoo.com>
2019-04-05 11:21:27 -0500

    Add project description, URL, and plugins.

--- src/blottsbooks/core.clj
+++ src/blottsbooks/core.clj
@@ -1,25 +1,25 @@
-(ns blottsbooks.core​
-  (:require blottsbooks.pricing)​
+(ns blottsbooks.core
+  (:require [blottsbooks.pricing])
   (:gen-class))
 
-(defn default-main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+;(defn default-main
+;  "I don't do a whole lot ... yet."
+;  [& args]
+;  (println "Hello, World!"))
 
 (defn say-welcome
   "Say welcome."
   [what]
   (println "Welcome to" what "!"))
 
-(defn -main []
+(defn -main
   "Executable entry point"
   []
   (say-welcome "Blott's Books")
   (println
-    (blottsbooks.pricing/discount-price {:title ​"Emma"​ :price 9.99})))
+    (blottsbooks.pricing/discount-price {:title "Emma" :price 9.99})))
 
-(defn prnint-greeting
+(defn print-greeting
   "Use a different greeting for preferred customers."
   [preferred-customer]
   (if preferred-customer
